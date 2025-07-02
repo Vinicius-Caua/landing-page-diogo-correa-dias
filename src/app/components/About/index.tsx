ed078253c3e61d70f2ffc304b0b4bae884510fb7
Vinicius <vinipg1@hotmail.com>
2025-07-02 19:04:22 -0300

    feat: criando componente about

--- src/app/components/About/index.tsx
+++ src/app/components/About/index.tsx
@@ -3,16 +3,32 @@ import Image from "next/image";
 
 function AboutComponent() {
   return (
-    <section className="w-full h-screen -mt-34">
-    <div className="flex justify-end">
-      <Image
-        src="/papers-about.png"
-        alt="Papeis com título 'Sobre Mim...'"
-        width={684}
-        height={472}
-        className="object-cover relative z-20"
-      />
-    </div>
+    <section className="w-full h-full flex flex-col">
+      <div className="">
+        <div className="flex justify-end -mt-32">
+          <Image
+            src="/papers-about.png"
+            alt="Papeis com título 'Sobre Mim...'"
+            width={684}
+            height={472}
+          />
+        </div>
+        <div className="flex justify-between items-center h-full pr-58">
+          <Image
+            src="/biografia-about.png"
+            alt="Papeis com biografia do autor"
+            width={800}
+            height={1158}
+          />
+          <Image
+            src="/retrato-about.png"
+            alt="Papeis com retrato do autor"
+            width={700}
+            height={780}
+            className="mb-52"
+          />
+        </div>
+      </div>
     </section>
   );
 }
